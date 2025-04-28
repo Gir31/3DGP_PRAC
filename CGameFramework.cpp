e2c8b260e37a8e0159919dd1c97d0105cb2ec128
JHG@LT <rpg2657@gmail.com>
2025-04-28 15:35:12 +0900

    따라하기 시작하자

---
+++ CGameFramework.cpp
@@ -0,0 +1,35 @@
+#include "CGameFramework.h"
+#include "stdafx.h"
+
+CGameFramework::CGameFramework()
+{
+	m_pdxgiFactory = NULL;
+	m_pdxgiSwapChain = NULL;
+	m_pd3dDevice = NULL;
+
+	m_pd3dCommandAllocator = NULL;
+	m_pd3dCommandQueue = NULL;
+	m_pd3dPipelineState = NULL;
+	m_pd3dCommandList = NULL;
+
+	for (int i = 0; i < m_nSwapChainBuffers; i++) m_ppd3dRenderTargetBuffers[i] = NULL;
+	m_pd3dRtvDescriptorHeap = NULL;
+	m_nRtvDescriptorIncrementSize = 0;
+
+	m_pd3dDepthStencilBuffer = NULL;
+	m_pd3dDsvDescriptorHeap = NULL;
+	m_nDsvDescriptorIncrementSize = 0;
+
+	m_nSwapChainBufferIndex = 0;
+
+	m_hFenceEvent = NULL;
+	m_pd3dFence = NULL;
+	m_nFenceValue = 0;
+
+	m_nWndClientWidth = FRAME_BUFFER_WIDTH;
+	m_nWndClientHeight = FRAME_BUFFER_HEIGHT;
+}
+
+CGameFramework::~CGameFramework()
+{
+}
